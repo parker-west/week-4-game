3a6870475032586331b11f8387a9e343bbe2f11f
parker-west <mail.parker@gmail.com>
2017-03-21 23:17:35 -0500

    basic game logic working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,8 +16,12 @@ $(document).ready(function() {
 	var playerAttack = 0;
 	var enemyAttack = 0;
 
+	var fighters = ["one","two","three"];
 
 
+	//Hide buttons on initialization
+	$("#reset").hide();
+	$("#attack").hide();
 
 	//Set event onclick event listener
 
@@ -26,46 +30,89 @@ $(document).ready(function() {
 
 	    // gets the id of clicked div with class of "players" within "select-player" div.
 	    id = event.target.id;
-	    //Removes class of clicked target. 
+	    //Remove class of clicked target. 
 	    $(event.target).removeClass("players");
+	    //Add playerFigter class.
 	    $(event.target).addClass("playerFighter");
 	    //Sets player health.
-	    playerHealth = 100;
+	    playerHealth = $(event.target).attr("data-value");
+	    console.log(playerHealth);
 	    //Appends target to "#your-character" div.
 	    $(event.target).appendTo("#your-character");
 	    //Appends all other ".player" divs to "#enemy"
 	    $(".players").appendTo("#enemy");
+	    
 
 	});
 
 	//click to move enemey to defender div.
 	$('#enemy').on('click', function(event) {
 
-		$(event.target).addClass("enemyFighter");
-		enemyHealth = 100;
+		$(event.target).addClass("enemy-fighter");
+		enemyHealth = $(event.target).attr("data-value");
+		console.log(enemyHealth);
 		id = event.target.id;
 		$(event.target).appendTo("#defender");
+		$("#attack").show();
 
 	});
 
 		//On "attack" button click...
 	$('#attack').on('click', function() {
 
+		$("#reset").hide();
+
+		var attackPower = 80;
+
 		if (enemyHealth > 0 && playerHealth > 0 )  {
-			enemyHealth -= (Math.floor(Math.random() * 20) + 1);
-			playerHealth -= (Math.floor(Math.random() * 20) + 1);
+			enemyHealth -= (Math.floor(Math.random() * attackPower) + 10);
+			$(".enemy-fighter > span").html(enemyHealth);
+			playerHealth -= (Math.floor(Math.random() * 10) + 1);
+			$(".playerFighter > span").html(playerHealth);
 		}	
 
 		if (playerHealth <= 0) {
-			$(".playerFighter").hide();	
+			$(".playerFighter").hide();
+			$("#reset").show();
+			$('#reset').click(function() {
+				location.reload();
+			});
+			//display reset button
 		}	
+		
 		if (enemyHealth <= 0) {
-			$(".enemyFighter").hide();
-		} 
+			$(".enemy-fighter").hide();
+			$("#reset").hide();
+			attackPower += 10;
+			fighters.pop();
+			$("#attack").hide();
+				
+			//print to select new enemy.
+			console.log(attackPower);
+		}
+
+		if (fighters.length === 0) {
+				$("#reset").show();
+				$('#reset').click(function() {
+				location.reload();
+			});
+			}
+		
+		if (enemyHealth <= 0 && playerHealth <= 0) {
 			
+			console.log("Double Kill");
+			$("#reset").show();
+			$('#reset').click(function() {
+				location.reload();
+			});
+		}
+
+
 		
 		console.log("enemy " + enemyHealth);
 		console.log("hero " + playerHealth);
+		
+		
 
 	});
 
